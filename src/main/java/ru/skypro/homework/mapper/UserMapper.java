2040713260fe26ca84a1c0728febc37c91eb1c24
Oleg Kartashev <officinator@gmail.com>
2023-07-12 16:06:37 +0300

    add mapping User from RegisterReq

--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package ru.skypro.homework.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.skypro.homework.dto.RegisterReq;
 import ru.skypro.homework.dto.UserDTO;
 import ru.skypro.homework.entity.User;
 
@@ -10,4 +12,7 @@ public interface UserMapper {
     UserDTO toUserDTO(User user);
 
     User fromUserDTO(UserDTO user);
+
+    @Mapping(source = "req.username", target = "email")
+    User fromRegisterReq(RegisterReq req);
 }
